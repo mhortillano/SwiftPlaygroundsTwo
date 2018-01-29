57cc9ab8e8e56f66fc1c6d02cec942ae7091c963
mhortillano <mhortillano@github.com>
2018-01-29 10:39:37 -1000

    Completed SW2

--- PlaygroundTwo.playground/Contents.swift
+++ PlaygroundTwo.playground/Contents.swift
@@ -3,3 +3,30 @@
 import UIKit
 
 var str = "Hello, playground"
+
+var goodGrade = "I've been doing all my work"
+if goodGrade == "I've been doing all my work"
+{
+    print("I should be getting an A at this point")
+}
+
+//Evaluate the condition so it's False
+
+//Part 3, #2
+
+if goodGrade == "I'm not doing very well"
+    {
+    print("I should be getting an A at this point")
+    }
+
+// Part 4
+
+if goodGrade == "Ive been doing all my work"
+    {
+    print("Im doing well in class")
+    }
+
+else
+    {
+    "But am I really?"
+    }
